f82a1f100d39362bedc9ce2a5331dcbe74563e4d
Trinidad Araujo <trinidadandeli@gmail.com>
2017-04-05 09:02:13 -0700

    game logic

--- js/app.js
+++ js/app.js
@@ -1,43 +1,77 @@
 /*--variables---*/
-
 var titles = ['Lolita', 'The Sound and the Fury','Dracula','Great Expectations',
 'Hamlet','The Great Gatsby','Wuthering Heights'];
-
-
+var chosenWord;
 
 /*--event listeners---*/
-
-
-
+document.querySelectorAll('ul')[0].addEventListener('click', handleClick)
+document.getElementById('reset').addEventListener('click', init)
 
 
 /*--functions---*/
 
-
-
-
-
-
 function wordstoUnderscore(word) {
-var underscores = "";
-for (var i =0;  i < word.length; i++) {
-underscores += " _ "
+  var underscores = ""
+  for (var i =0;  i < word.length; i++) {
+    underscores += " _ "
+  }
+  return underscores
 }
-return underscores
-};
 
 
 function renderUnderscores(str){
-document.querySelector(".chosenword").innerHTML = str
-
-}
-
-function render(){
-  var i = Math.floor(Math.random() * 7);
-
-renderUnderscores(wordstoUnderscore(titles[i]));
-
+  document.querySelector(".chosenword").innerHTML = str
 }
 
 
-render();
+function handleClick(evt) {
+  var currentLetter = evt.target.innerHTML
+  var newUnderscores = isletterinChosenWord(currentLetter, chosenWord)
+  console.log('newUnd', newUnderscores)
+  document.querySelector('.chosenWord').innerHTML = newUnderscores
+
+}
+
+
+function isletterinChosenWord(letter,word) {
+  var underScorified = ""
+
+  if (word.indexOf(letter) !== -1) {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        underScorified+= word[i]
+      } else {
+        underScorified+= " _ "
+      }
+    }
+  }
+
+  return underScorified /*|| false*/
+
+}
+
+
+function init(){
+  chosenWord =  titles[Math.floor(Math.random() * 7)].toLowerCase()
+  render(chosenWord)
+}
+
+
+function render(chosenWord){
+  chosenWord =  titles[Math.floor(Math.random() * 7)].toLowerCase()
+  renderUnderscores(wordstoUnderscore(chosenWord))
+}
+
+
+init()
+
+
+
+
+
+
+
+
+
+
+
